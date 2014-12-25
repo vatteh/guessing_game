ae689bb54c7efdcd6560620a844260c3816cc985
Victor Atteh <victoratteh@gmail.com>
2014-12-25 04:31:53 -0500

    added visual list of hot and cold answers

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function GuessingGame(mainBox) {
 	this.mainBox = mainBox;
 	this.guessesLeft = 5;
+	this.guessesMade = [];
 	this.myNumber = this.randomNumber();
 	this.inputField = this.mainBox.find('#input_field');
-	this.guessCountText = this.mainBox.find('#guess-count')
-}
+	this.guessCountText = this.mainBox.find('#guess-count');
+	this.guessButton = this.mainBox.find('.guess_button');
+	this.circles = this.mainBox.find('.circleBase').map(function() {
+    	return $(this);
+  	});
+};
 
 GuessingGame.prototype.randomNumber = function() {
 	return Math.floor(Math.random() * 100) + 1;
@@ -29,24 +34,43 @@ GuessingGame.prototype.submitGuess = function() {
    		});
 
    		return;
-	} 
+	} else if (this.guessesMade.indexOf(guess) > -1) {
+		this.guessCountText.html("You've already submitted this number!").css('color', 'red').delay(150).fadeIn(function () {
+   			$(this).css("color", "white");
+   		});
+
+   		return;
+	}
 
 	this.guessesLeft--;
+	this.guessesMade.push(guess);
 	console.log(this.myNumber);
 
 	if (guess == this.myNumber) {
 		this.guessedTheNumber();
+		this.circles[this.guessesMade.length-1].attr("id", "circleType-Win");
+		console.log(this.guessesMade.length-1);
 	} else {
 		if (this.guessesLeft == 4) {
-			this.guessCountText.html("Four guesses left");
+			this.guessCountText.html(this.circleType(guess)[0] + this.circleType(guess)[1] + "Four guesses left");
+			this.guessButton.attr("id", "guess-4");
+			this.circles[0].attr("id", this.circleType(guess)[2]).html(guess);
 		} else if (this.guessesLeft == 3) {
-			this.guessCountText.html("Three guesses left");
+			this.guessCountText.html(this.circleType(guess)[0] + this.circleType(guess)[1] + "Three guesses left");
+			this.guessButton.attr("id", "guess-3");
+			this.circles[1].attr("id", this.circleType(guess)[2]).html(guess);
 		} else if (this.guessesLeft == 2) {
-			this.guessCountText.html("Two guesses left");
+			this.guessCountText.html(this.circleType(guess)[0] + this.circleType(guess)[1] + "Two guesses left");
+			this.guessButton.attr("id", "guess-2");
+			this.circles[2].attr("id", this.circleType(guess)[2]).html(guess);
 		} else if (this.guessesLeft == 1) {
-			this.guessCountText.html("Last chance");
+			this.guessCountText.html(this.circleType(guess)[0] + this.circleType(guess)[1] + "Last chance");
+			this.guessButton.attr("id", "guess-1");
+			this.circles[3].attr("id", this.circleType(guess)[2]).html(guess);
 		} else {
-			this.guessCountText.html("Game over. Play again?");
+			this.guessCountText.html(this.circleType(guess)[0] + "Game over. Play again?");
+			this.guessButton.attr("id", "guess-0");
+			this.circles[4].attr("id", this.circleType(guess)[2]).html(guess);
 		}
 	}
 };
@@ -55,6 +79,11 @@ GuessingGame.prototype.restart = function() {
 	this.guessesLeft = 5;
 	this.myNumber = this.randomNumber();
 	this.guessCountText.html("You get 5 tries to guess my number").css("font-size", "24px").attr("id", "guess-count");
+	this.guessButton.attr("id", "guess-5");
+	this.guessesMade = [];
+	this.circles = this.circles.map(function() {
+    	return this.attr("id", "").html("");
+  	});
 };
 
 GuessingGame.prototype.hint = function() {
@@ -65,11 +94,50 @@ GuessingGame.prototype.hint = function() {
 };
 
 GuessingGame.prototype.guessedTheNumber = function() {
-	this.guessCountText.html("You guessed my number!").css("font-size", "32px").attr("id", "guess-count-win");
+	this.guessCountText.html(this.myNumber + " is it! You guessed my number!").attr("id", "guess-count-win");
 	this.guessesLeft = 0;
 };
 
-GuessingGame.prototype.isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+GuessingGame.prototype.isNumber = function(number) {
+  return !isNaN(parseFloat(number)) && isFinite(number);
 };
 
+GuessingGame.prototype.circleType = function(guess) {
+  var result = [];
+  var hotness;
+  var guessUpDown;
+
+  if (Math.abs(guess - this.myNumber) <= 5) {
+  	hotness = guess + " is very hot! ";
+  	guessUpDown = guess > this.myNumber ? "Guess lower. " : " Guess higher. "; 
+  	result.push(hotness);
+  	result.push(guessUpDown);
+  	result.push("circleType-VeryHot");
+  } else if (Math.abs(guess - this.myNumber) <= 10) {
+  	hotness = guess + " is hot! ";
+  	guessUpDown = guess > this.myNumber ? "Guess lower. " : " Guess higher. "; 
+  	result.push(hotness);
+  	result.push(guessUpDown);
+  	result.push("circleType-Hot");
+  } else if (Math.abs(guess - this.myNumber) <= 30) {
+  	hotness = guess + " is warm! ";
+  	guessUpDown = guess > this.myNumber ? "Guess lower. " : " Guess higher. "; 
+  	result.push(hotness);
+  	result.push(guessUpDown);
+  	result.push("circleType-Warm");
+  } else if (Math.abs(guess - this.myNumber) <= 50) {
+  	hotness = guess + " is cold! ";
+  	guessUpDown = guess > this.myNumber ? "Guess lower. " : " Guess higher. "; 
+  	result.push(hotness);
+  	result.push(guessUpDown);
+  	result.push("circleType-Cold");
+  } else { 
+  	hotness = guess + " is very cold! ";
+  	guessUpDown = guess > this.myNumber ? "Guess lower. " : " Guess higher. "; 
+  	result.push(hotness);
+  	result.push(guessUpDown);
+  	result.push("circleType-VeryCold");
+  }
+
+  return result;
+};
